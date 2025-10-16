9b4ccac4d1130b74d9de7ba93c11a36e6da59087
Ano <81051693+Mushroom-Ano@users.noreply.github.com>
2025-10-16 15:35:57 +0100

    CREATE: helloyou function with firstname as parameter git push

--- src/main/java/com/qa/PersonProject/PersonProjectApplication.java
+++ src/main/java/com/qa/PersonProject/PersonProjectApplication.java
@@ -3,6 +3,7 @@ package com.qa.PersonProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,5 +18,11 @@ public class PersonProjectApplication {
     public String sayHello(){
         return "Hello World";
     }
+
+    @GetMapping("/helloyou")
+    public String sayHelloYou(@RequestParam(name="firstname", defaultValue = "Everyone") String firstname) {
+        return String.format("Hello %s!", firstname);
+    }
+
 
 }
