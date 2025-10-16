6edd60020c09c06650c0b187a69465fc6e02c243
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-10-16 15:53:50 +0100

    CREATE: created first post mapping /create to add person to people list

--- src/main/java/com/qa/PersonProject/controllers/PersonController.java
+++ src/main/java/com/qa/PersonProject/controllers/PersonController.java
@@ -1,14 +1,27 @@
 package com.qa.PersonProject.controllers;
 
+import com.qa.PersonProject.entities.Person;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
 
+    private List<Person> people = new ArrayList<>();
+
     @GetMapping("/health")
     public String health() {
         return "Testing 1, 2, 3";
+    }
+
+    @PostMapping("/create")
+    public boolean addPerson(@RequestBody Person person) {
+        return this.people.add(person);
     }
 
 }
